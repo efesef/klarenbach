77c013a8948fa01038642ff9af733550784e1692
efesef <f.schmidtfischbach@gmail.com>
2022-11-16 14:00:57 +0100

    adding plotly back

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
-def homepage(): 
-   return(POSTGRES_URL)
+ def homepage():
+    df = pd.DataFrame({
+       'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 
+       'Bananas'],
+       'Amount': [4, 1, 2, 2, 4, 5],
+       'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    })
+    fig = px.bar(df, x='Fruit', y='hallo'', color='City', 
+       barmode='group')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('plotly.html', graphJSON=graphJSON)
  
 if __name__ == "__main__": 
     app.run(debug=True)
