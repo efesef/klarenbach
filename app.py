7e7a15ad05e5f1a749b1d72260613ecfcd9b8195
efesef <f.schmidtfischbach@gmail.com>
2022-11-16 13:45:09 +0100

    adding config file to leverage secrets in environment

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ import json
 import plotly
 import plotly.express as px
 app = Flask(__name__)
-
-db_name=os.environ.get("POSTGRES_NAME")
-db_reader=os.environ.get("POSTGRES_READER")
-db_pw=os.environ.get("POSTGRES_PW")
-
-PG_URL=f"postgresql://{db_reader}:{db_pw}@db:5432/{db_name}"
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def homepage(): 
-   return(PG_URL)
+   return(POSTGRES_URL)
  
 if __name__ == "__main__": 
     app.run(debug=True)
