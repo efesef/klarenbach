f0630fc71f6c416fd7d9d1d32dd010164a68c717
efesef <f.schmidtfischbach@gmail.com>
2022-11-17 16:21:49 +0100

    pass environmental variables to both docker containers via docker-compose

--- app/app.py
+++ app/app.py
@@ -3,12 +3,13 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import os 
+
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
 
 @app.route('/')
 def homepage():
-   return app.config["POSTGRES_URL"]
+   return os.environ["POSTGRES_URL"]
    
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0', port=5001)
