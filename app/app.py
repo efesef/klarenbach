b94aa8689cab235c890e699b239e264001d81952
efesef <f.schmidtfischbach@gmail.com>
2022-11-17 16:29:12 +0100

    lint

--- app/app.py
+++ app/app.py
@@ -3,13 +3,23 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
-import os 
+import os
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
-   return os.environ["POSTGRES_URL"]
-   
-if __name__ == "__main__": 
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # connect to postgres database.
+    conn = psycopg2.connect(
+        dbname=os.environ["POSTGRES_DB"],
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+    )
+
+    return os.environ["POSTGRES_URL"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
